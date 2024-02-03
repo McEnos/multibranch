2e2ddd3081b6fb5d41d63f831e8c4dc61047bbff
joshua.odiwuor <joshua.odiwuor@turnkeyafrica.com>
2024-02-03 09:05:54 +0300

    Adds Observability

--- service-2/src/main/java/com/example/service2/Service2Application.java
+++ service-2/src/main/java/com/example/service2/Service2Application.java
@@ -1,13 +1,20 @@
 package com.example.service2;
 
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Service2Application {
 //triggger maven build
     public static void main(String[] args) {
         SpringApplication.run(Service2Application.class, args);
+    }
+    @Bean
+    ObservedAspect observedAspect(ObservationRegistry registry) {
+        return new ObservedAspect(registry);
     }
 
 }
